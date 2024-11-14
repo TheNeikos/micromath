042cfe597c0274f9052cd98fc055052c5db24b14
Marcel Müller <neikos@neikos.email>
2024-11-14 15:41:13 +0100

    Allow mean on iterator of f32 refs
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- src/statistics/mean.rs
+++ src/statistics/mean.rs
@@ -1,5 +1,5 @@
 /// Arithmetic mean
-pub trait Mean {
+pub trait Mean<I> {
     /// Result type
     type Result;
 
@@ -7,7 +7,7 @@ pub trait Mean {
     fn mean(self) -> Self::Result;
 }
 
-impl<I> Mean for I
+impl<I> Mean<f32> for I
 where
     I: Iterator<Item = f32>,
 {
@@ -26,12 +26,25 @@ where
     }
 }
 
+impl<'a, I> Mean<&f32> for I
+where
+    I: Iterator<Item = &'a f32>,
+{
+    type Result = f32;
+
+    fn mean(self) -> f32 {
+        self.copied().mean()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::Mean;
 
     #[test]
     fn mean_test() {
-        assert_eq!([1.0, 3.0, 5.0].iter().cloned().mean(), 3.0);
+        assert_eq!([1.0, 3.0, 5.0].iter().mean(), 3.0);
+
+        assert_eq!([1.0, 3.0, 5.0].into_iter().mean(), 3.0);
     }
 }
